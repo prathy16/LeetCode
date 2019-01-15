7cd3c751821d540c6230a6c6736a7d36a9188796
Prathyusha <31429359+prathy16@users.noreply.github.com>
2019-01-14 22:28:58 -0700

    Create track_moves_on_keyboard.cpp

---
+++ track_moves_on_keyboard.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+string moves(string word)
+{
+    pair<int, int> curPos(0,0);
+    string output;
+    int x, y, temp;
+
+    for(char c: word)
+    {
+        x = (c - 'a')/7;
+        y = (c - 'a')%7;
+
+        temp = x - curPos.first;
+        if(temp < 0){
+          output.insert(output.end(), -temp, 'U');
+        }
+        else if(temp > 0){
+          output.insert(output.end(), temp, 'D');
+        }
+
+        temp = y - curPos.second;
+        if(temp < 0){
+          output.insert(output.end(), -temp, 'L');
+        }
+        else if(temp > 0){
+          output.insert(output.end(), temp, 'R');
+        }
+
+        curPos = pair<int, int>(x, y);
+    }
+
+    return output;
+}
+
+int main()
+{
+  string output = moves("prathy");
+  cout << output << endl;
+  return 0;
+}
